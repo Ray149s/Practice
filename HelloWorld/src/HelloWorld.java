7d517e379d1eca39e601925b8074e2b65ddf47f0
gabriel-145s <gluna5199@gmail.com>
2021-10-09 14:08:33 -0700

    Added For loop

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,6 +6,9 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		// what is going on here
+		for (int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
